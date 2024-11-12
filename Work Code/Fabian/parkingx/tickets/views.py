71e5016f3df60f494b0854a1a8a68db88d05628b
TrunksMC <166747493+TrunksMC@users.noreply.github.com>
2024-11-11 22:04:21 -0300

    Project 1.2
    Se creo el CRUD de Protocolos de Seguridad.

---
+++ Work Code/Fabian/parkingx/tickets/views.py
@@ -0,0 +1,99 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+
+from .models import Rol, Usuario, Protocolo
+from .forms import RolForm, UsuarioForm, UserUpdateForm, ProtocoloForm
+
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+# Create your views here.
+
+# ----------------------------------- #
+
+@login_required
+def Home(request):
+    return render(request, 'tickets/base.html')
+
+# ----------------------------------- #
+
+class CreateRol(CreateView):
+    model = Rol
+    form_class = RolForm
+    template_name = 'tickets/createrol.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
+# ----------------------------------- #
+    
+class CreateProtocolo(CreateView, ListView):
+    model = Protocolo
+    form_class = ProtocoloForm
+    template_name = 'tickets/protocolo.html'
+    success_url = reverse_lazy('protocolo')
+    context_object_name = 'protocolos'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
+class UpdateProtocolo(UpdateView):
+    model = Protocolo
+    form_class = ProtocoloForm
+    template_name = 'tickets/protocolo_update.html'
+    success_url = reverse_lazy('protocolo')
+
+class DeleteProtocolo(DeleteView):
+    model = Protocolo
+    template_name = 'tickets/protocolo_delete.html'
+    success_url = reverse_lazy('protocolo')
+    context_object_name = 'protocolos'
+
+# ----------------------------------- #
+
+class CreateUsuario(UserPassesTestMixin, CreateView):
+    model = Usuario
+    form_class = UsuarioForm
+    template_name = 'tickets/usercreate.html'
+    success_url = reverse_lazy('home')
+
+    def test_func(self):
+        return self.request.user.rol.name == 'root'
+    
+    def form_valid(self, form):
+        user = form.save()
+        return super().form_valid(form)
+    
+class ListUsuario(ListView):
+    model = Usuario
+    template_name = 'tickets/userlist.html'
+    context_object_name = 'usersys'
+
+class UpdateUsuario(UpdateView):
+    model = Usuario
+    form_class = UserUpdateForm
+    template_name = 'tickets/userupdate.html'
+    success_url = reverse_lazy('home')
+    context_object_name = 'usuario'
+
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get('pk')
+        return Usuario.objects.get(pk=pk)
+    
+class DeleteUsuario(DeleteView):
+    model = Usuario
+    template_name = 'tickets/userdelete.html'
+    success_url = reverse_lazy('userlist')
+    context_object_name = 'usuario'
+
+# ----------------------------------- #
+
+def custom_logout(request):
+    logout(request)  
+    return redirect('login') 
